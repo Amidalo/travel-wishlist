a3bbc7d9a04726665f67990f3079baa726bb4298
Kovalenko <amidalo148@gmail.com>
2025-09-20 22:25:53 +0700

    initial commit

---
+++ travel-wishlist-backend/src/main/java/wild/yellow/travelwishlistbackend/TravelWishlistBackendApplication.java
@@ -0,0 +1,13 @@
+package wild.yellow.travelwishlistbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelWishlistBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelWishlistBackendApplication.class, args);
+    }
+
+}
