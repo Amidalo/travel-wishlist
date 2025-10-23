357b50335d9543661c8d0b9a664085fb764ff47b
Kovalenko <amidalo148@gmail.com>
2025-10-23 18:03:29 +0700

    Add migration: Create_DB, Add_users, Add_destinations

---
+++ travel-wishlist-backend/src/main/resources/db/migration/V2__Add_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO consumer (username, password, created_at) VALUES
+    ('alex', '$2a$04$3t5U2xPWyAFhEwvoyXvrjO5uDyqoK.R90VzZrDfPYUVJn6ec5kAxG', '2025-09-26 13:09:01.816596'),
+    ('john', '$2a$04$pJ.IkP9mUFJZeUmkuD8/eu8T.3TE0Y9cCYo87wnpzmK4T0pB0t1K2', '2025-09-26 13:09:08.582887'),
+    ('Lola', '$2a$04$yAv0uYMQYu3iEI24XF39iOcW45tbXEL/oItM6fNqQ/LAyM6dYHrKm', '2025-09-26 13:09:23.697563');
---
+++ travel-wishlist-backend/src/main/resources/db/migration/V3__Add_destinations.sql
@@ -0,0 +1,4 @@
+INSERT INTO destination (name, description, status, consumer_id, created_at) VALUES
+('Tomsk', 'I wanna go to Tomsk!', 'PLANNED', '1',  '2025-09-26 13:09:01.816596'),
+('Tomsk', 'I wanna go to Tomsk!', 'VISITED', '2',  '2025-09-27 13:09:01.816596'),
+('Tomsk', 'I wanna go to Tomsk!', 'PLANNED', '3',  '2025-09-28 13:09:01.816596');
